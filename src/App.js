481468c0d73916420e88ff424dc9b56df1ccc350
Aung Naing <aungnaing.brian@gmail.com>
2026-05-13 17:35:25 +0400

    c6end

--- src/App.js
+++ src/App.js
@@ -1,48 +1,17 @@
 
-// import { useState } from 'react';
-// import './App.css';
-// import TripList from './components/TripList/index.js'
-
-
-// function App() {
-  
-//   let [show,setShow] = useState(true);
-
-//   return (
-//     <>
-//       <button onClick={()=>setShow(false)}>Hite Trips</button>
-//       {show && <TripList/>}
-//     </>
-    
-//   );
-// }
-
-// export default App;
-
-
-//C6ep37
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import TripList from './components/TripList/index.js'
 
 
 function App() {
   
-  let [data,setData] = useState('my data');
-
-  let myFunction=()=>{
-    setData('update Data'); //render->stop because of same date.
-  }
-
-  useEffect(()=>{
-    myFunction();
-    console.log('running');
-  },[myFunction])
+  let [show,setShow] = useState(true);
 
   return (
     <>
-     <h1>{data}</h1>
-     <TripList/>
+      <button onClick={()=>setShow(false)}>Hite Trips</button>
+      {show && <TripList/>}
     </>
     
   );
@@ -50,5 +19,36 @@ function App() {
 
 export default App;
 
-//data = 'my data' -> first render
-//data = 'update data' -> seconder render
+
+// //C6ep37
+// import { useEffect, useState } from 'react';
+// import './App.css';
+// import TripList from './components/TripList/index.js'
+
+
+// function App() {
+  
+//   let [data,setData] = useState('my data');
+
+//   let myFunction=()=>{
+//     setData('update Data'); //render->stop because of same date.
+//   }
+
+//   useEffect(()=>{
+//     myFunction();
+//     console.log('running');
+//   },[myFunction])
+
+//   return (
+//     <>
+//      <h1>{data}</h1>
+//      <TripList/>
+//     </>
+    
+//   );
+// }
+
+// export default App;
+
+// //data = 'my data' -> first render
+// //data = 'update data' -> seconder render
