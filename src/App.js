a63c89f7214e4557a44473369bb40b5eedad80fb
Aung Naing <aungnaing.brian@gmail.com>
2026-05-13 17:34:36 +0400

    c6ep37

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
 
-import { useState } from 'react';
+// import { useState } from 'react';
+// import './App.css';
+// import TripList from './components/TripList/index.js'
+
+
+// function App() {
+  
+//   let [show,setShow] = useState(true);
+
+//   return (
+//     <>
+//       <button onClick={()=>setShow(false)}>Hite Trips</button>
+//       {show && <TripList/>}
+//     </>
+    
+//   );
+// }
+
+// export default App;
+
+
+//C6ep37
+import { useEffect, useState } from 'react';
 import './App.css';
 import TripList from './components/TripList/index.js'
 
 
 function App() {
   
-  let [show,setShow] = useState(true);
+  let [data,setData] = useState('my data');
+
+  let myFunction=()=>{
+    setData('update Data'); //render->stop because of same date.
+  }
+
+  useEffect(()=>{
+    myFunction();
+    console.log('running');
+  },[myFunction])
 
   return (
     <>
-      <button onClick={()=>setShow(false)}>Hite Trips</button>
-      {show && <TripList/>}
+     <h1>{data}</h1>
+     <TripList/>
     </>
     
   );
 }
 
 export default App;
+
+//data = 'my data' -> first render
+//data = 'update data' -> seconder render
